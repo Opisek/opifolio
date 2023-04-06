c2d6aa3e4a60329b0195fcd0a53e9a3904439561
Opisek <darowski.kacper@gmail.com>
2023-04-06 11:17:24 +0200

    merged frontend-typescript

--- src/frontend/scripts/webpack.config.js
+++ src/frontend/scripts/webpack.config.js
@@ -1,43 +1,58 @@
-const path = require('path');
-const paths = require('./paths')
+import { join, parse } from "path";
+import { rootDir, distDir, htmlDir } from "./paths.js";
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ImageMinimizerPlugin from "image-minimizer-webpack-plugin";
 
-module.exports = (htmlFiles) => ({
+export default (htmlFiles) => ({
     mode: "production",
-    entry: path.join(paths.rootDir, "entry.js"),
+    entry: join(rootDir, "entry.js"),
     output: {
-        path: paths.distDir,
-        assetModuleFilename: 'static/[hash][ext]'
+        path: distDir,
+        assetModuleFilename: "static/[hash][ext]"
     },
     module: {
         rules: [
             {
                 test: /\.ejs$/i,
+                exclude: /node_modules/,
                 use: ["html-loader", "template-ejs-loader"]
             },
             {
                 test: /\.css$/i,
+                exclude: /node_modules/,
                 type: "asset/resource",
                 generator : {
-                    filename : 'css/[hash][ext]',
+                    filename : "css/[hash][ext]",
+                }
+            },
+            {
+                test: /\.ts$/i,
+                exclude: /node_modules/,
+                type: "asset/resource",
+                use: "ts-loader",
+                generator : {
+                    filename : "js/[hash].js",
                 }
             },
             {
                 test: /\.(png|jpe?g|webp|gif|svg|)$/i,
+                exclude: /node_modules/,
                 type: "asset",
                 generator : {
-                    filename : 'images/[hash][ext]',
+                    filename : "images/[hash][ext]",
                 }
             }
         ],
     },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
     plugins: htmlFiles.map(
         file => new HtmlWebpackPlugin({
-            filename: `html/${path.parse(file).name}.html`,
-            template: path.join(paths.htmlDir, file),
+            filename: `html/${parse(file).name}.html`,
+            template: join(htmlDir, file),
         })
     ).concat([
     ]),
@@ -59,8 +74,8 @@ module.exports = (htmlFiles) => ({
                 ],
             })
         ]
-    }
-})
+    },
+});
 
 // https://www.npmjs.com/package/image-minimizer-webpack-plugin
 // https://stackoverflow.com/questions/67250804/html-loader-is-not-getting-correct-img-src-path
