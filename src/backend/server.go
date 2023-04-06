b2cba7871ef5cfe9c8d95992bb77ae36913993e9
Opisek <darowski.kacper@gmail.com>
2023-04-06 21:00:11 +0200

    backend handle gzip

--- src/backend/server.go
+++ src/backend/server.go
@@ -4,33 +4,61 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 )
+
+//
+// Data Types
+//
 
 type Redirect struct {
 	Pages []string
 	Url   string
 }
 
+//
+// Main
+//
+
 func main() {
-	// get html files so we can determine 404 without expensive disk reads
+	// get data we need
+	htmlFilesMap := getHtmlFilesMap()
+	redirectsMap := getRedirectsMap()
+
+	// handle static resources
+	http.Handle("/css/", handleStatic("text/css", http.StripPrefix("/css/", http.FileServer(http.Dir("public/css")))))
+	http.Handle("/js/", handleStatic("text/javascript", http.StripPrefix("/js/", http.FileServer(http.Dir("public/js")))))
+	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("public/images"))))
+
+	// handle views and redirects
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { handleView(w, r, &htmlFilesMap, &redirectsMap) })
+
+	// start the server
+	fmt.Println("Listening on port " + os.Getenv("PORT"))
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+}
+
+//
+// Data Loader
+//
+
+func getHtmlFilesMap() map[string]bool {
 	htmlFiles, err := os.ReadDir("public/html")
 	if err != nil {
 		panic(err)
 	}
+
 	htmlFilesMap := map[string]bool{}
 	for _, file := range htmlFiles {
 		htmlFilesMap[file.Name()] = true
 	}
 
-	// static data
-	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("public/css"))))
-	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("public/js"))))
-	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("public/images"))))
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("public/static"))))
+	return htmlFilesMap
+}
 
-	// redirects
+func getRedirectsMap() map[string]string {
 	redirectsJson, err := os.ReadFile("config/redirects.json")
 	if err != nil {
 		panic(err)
@@ -49,29 +77,84 @@ func main() {
 		}
 	}
 
-	// page handling
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		path := r.URL.Path[1:] + ".html"
-		if path == ".html" {
-			path = "index.html"
+	return redirectsMap
+}
+
+//
+// Compression Determination
+//
+
+func getCompressionType(r *http.Request) string {
+	return "gzip"
+}
+
+func supportsCompression(r *http.Request) bool {
+	return strings.Contains(r.Header.Get("Accept-Encoding"), getCompressionType(r))
+}
+
+func getCompressedExtension(r *http.Request) string {
+	if supportsCompression(r) {
+		return ".gz"
+	} else {
+		return ""
+	}
+}
+
+//
+// File Handling
+//
+
+func handleStatic(contentType string, h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// if no encoding is supported, immediatel forward to the next handler
+		if !supportsCompression(r) {
+			h.ServeHTTP(w, r)
+			return
 		}
 
-		if htmlFilesMap[path] {
-			http.ServeFile(w, r, "public/html/"+path)
-		} else {
-			split := strings.Split(r.URL.Path[1:], "/")
-			url, exists := redirectsMap[split[0]]
-			if exists {
-				if len(split) != 1 {
-					url += "/" + r.URL.Path[len(split[0])+2:]
-				}
-				http.Redirect(w, r, url, http.StatusPermanentRedirect)
-			} else {
-				http.ServeFile(w, r, "public/html/404.html")
-			}
-		}
+		// add the compression extension to the path
+		r2 := new(http.Request)
+		*r2 = *r
+		r2.URL = new(url.URL)
+		*r2.URL = *r.URL
+		r2.URL.Path = r.URL.Path + getCompressedExtension(r)
+
+		// set matching compression encoding
+		w.Header().Set("Content-Encoding", getCompressionType(r))
+		w.Header().Set("Content-Type", contentType)
+
+		// forward request to the next handler in the chain
+		h.ServeHTTP(w, r2)
 	})
+}
 
-	fmt.Println("Listening on port " + os.Getenv("PORT"))
-	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+func handleView(w http.ResponseWriter, r *http.Request, htmlFilesMap *map[string]bool, redirectsMap *map[string]string) {
+	// get the requested filename
+	path := r.URL.Path[1:]
+	if path == "" {
+		path = "index"
+	}
+
+	if !(*htmlFilesMap)[path+".html"] {
+		// if no such page exists, check if there is a matching redirect
+		split := strings.Split(r.URL.Path[1:], "/")
+		url, exists := (*redirectsMap)[split[0]]
+		if exists {
+			if len(split) != 1 {
+				url += "/" + r.URL.Path[len(split[0])+2:]
+			}
+			http.Redirect(w, r, url, http.StatusPermanentRedirect)
+			return
+		} else {
+			// if no matching redirect exists either, show the 404 page
+			path = "404"
+		}
+	}
+
+	// finally, send a file if the user had not been redirected
+	if supportsCompression(r) {
+		w.Header().Set("Content-Encoding", getCompressionType(r))
+		w.Header().Set("Content-Type", "text/html")
+	}
+	http.ServeFile(w, r, "public/html/"+path+".html"+getCompressedExtension(r))
 }
